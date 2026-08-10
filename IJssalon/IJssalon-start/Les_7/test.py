5bd5c293fe1bd196f0c0bb57949e2b5b7487b35c
Lizenko <lizenko.official@gmail.com>
2026-08-10 19:48:45 +0200

    Add IJssalon-start

---
+++ IJssalon/IJssalon-start/Les_7/test.py
@@ -0,0 +1,46 @@
+#0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
+
+
+mijn_string = "2000"
+Mijn_int = int(2000)
+
+print(mijn_string+mijn_string)
+print(Mijn_int+Mijn_int)
+
+for c in "ijshoorentje":
+    print(c)
+
+print("202" + "20")
+
+#-------------------#
+
+print ("Mijn favoriete smaak is aardbijen" [5:10])
+
+smaak = "citroen"
+print(f"Ik hou van {smaak} ijs")
+print ("ijshoorentje".swapcase())
+
+woorden = ["Mijn, favoriete, smaak, is, aardbijen"]
+
+print(woorden[0])
+
+
+
+print("Mijn favoriete smaak is aardbij".replace("aardbij", "citroen"))
+
+woord = "chocolade"
+aantal = 9
+print(f"er zitten {aantal} letters in het woord {woord}")
+print("Mijn favoriete smaak is aardbij".index("Mijn"))
+zin1 = "Dit is een aardbij"
+zin2 = zin1.swapcase()
+zin3 = zin1 +" " + zin2
+
+print(zin3)
+
+
+totaal = 0
+def mijn_functie():
+    a = 2
+    totaal = totaal + a
+    print (totaal)
